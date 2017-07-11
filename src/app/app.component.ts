81f47e48badfecfded2ba6653f3fa80f8d792777
Aubrey Franzen <afranzen@CSMBP0002.local>
2017-07-11 11:58:13 -0400

    tennis scoring app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+
+export class AppComponent{
+    score: string
+    sets: string[]
+
+    constructor(private http: Http) {
+      this.http.get('http://localhost:8080/tennis/').subscribe(result => {
+          this.score = result.text();
+      })
+    }
+
+    onClickP1() {
+        this.http.get('http://localhost:8080/tennis/playerOne/').subscribe(result => {
+            this.score = result.text();
+        })
+    }
+
+    onClickP2() {
+        this.http.get('http://localhost:8080/tennis/playerTwo/').subscribe(result => {
+            this.score = result.text();
+        })
+    }
+
+    onClickNewMatch() {
+        this.http.get('http://localhost:8080/tennis/newMatch/').subscribe(result => {
+            this.score = result.text();
+        })
+    }
+
+    matchScore() {
+      this.http.get('http://localhost:8080/tennis/matchScore/').subscribe(result => {
+          this.sets = (result.toString()).split(",");
+      })
+    }
+
 }
