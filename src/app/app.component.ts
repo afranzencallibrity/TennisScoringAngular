c42ec87d04847e996e132af69eadcdeb792e0204
afranzencallibrity <afranzen@callibrity.com>
2018-02-12 15:04:35 -0500

    Updating application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,33 +12,44 @@ export class AppComponent{
     sets: string[]
 
     constructor(private http: Http) {
-      this.http.get('http://localhost:8080/tennis/').subscribe(result => {
-          this.score = result.text();
-      })
+        this.http.get('http://localhost:8080/tennis/').subscribe(result => {
+            this.score = result.text();
+        })
+        this.matchScore();
     }
 
     onClickP1() {
         this.http.get('http://localhost:8080/tennis/playerOne/').subscribe(result => {
             this.score = result.text();
         })
+        this.matchScore();
     }
 
     onClickP2() {
         this.http.get('http://localhost:8080/tennis/playerTwo/').subscribe(result => {
             this.score = result.text();
         })
+        this.matchScore();
     }
 
     onClickNewMatch() {
         this.http.get('http://localhost:8080/tennis/newMatch/').subscribe(result => {
             this.score = result.text();
+            this.sets = [];
         })
     }
 
     matchScore() {
       this.http.get('http://localhost:8080/tennis/matchScore/').subscribe(result => {
-          this.sets = (result.toString()).split(",");
+          this.sets = result.json();
       })
+    }
+
+    isGameOver() {
+        if(this.score === "Game over") {
+            return true;
+        }
+        return false;
     }
 
 }
